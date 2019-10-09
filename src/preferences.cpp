e3e6e893a2d6ecd7eefa74a9b53b882ce9ec6899
nightsky30 <6024315+nightsky30@users.noreply.github.com>
2019-10-09 09:17:12 -0500

    WIP:  Custom BGs displayed in prefs
    BGs displayed in prefs tab with labels and radio buttons

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -19,9 +19,15 @@
 
 #include "preferences.h"
 #include "ui_preferences.h"
+#include <iostream>
 #include <QIcon>
 #include <QString>
 #include <QVBoxLayout>
+#include <QResource>
+#include <QDir>
+#include <QVector>
+#include <QLabel>
+#include <QRadioButton>
 
 Preferences::Preferences(QWidget *parent) : QDialog (parent),
     ui(new Ui::Preferences)
@@ -35,35 +41,92 @@ Preferences::Preferences(QWidget *parent) : QDialog (parent),
     this->setWindowTitle("Koi-Koi Hanafuda - Preferences");
     this->setWindowIcon(QIcon(QString(":/icon/koi-2.svg")));
 
-//    //Create vertical box layout
-//    QVBoxLayout *vBoxLayout = new QVBoxLayout(this);
+    //    //Create vertical box layout
+    //    QVBoxLayout *vBoxLayout = new QVBoxLayout(this);
 
-//    //Set up about fonts
-//    QFont titleFont("Noto Sans", 12, QFont::Bold);
-//    QFont aboutFont("Arial", 9, QFont::Normal);
+    //    //Set up about fonts
+    //    QFont titleFont("Noto Sans", 12, QFont::Bold);
+    //    QFont aboutFont("Arial", 9, QFont::Normal);
 
-//    //Set up application name
-//    QLabel *appName = new QLabel(this);
-//    appName->setText(QString(tr("Koi-Koi Hanafuda")));
-//    appName->setFont(titleFont);
+    //    //Set up application name
+    //    QLabel *appName = new QLabel(this);
+    //    appName->setText(QString(tr("Koi-Koi Hanafuda")));
+    //    appName->setFont(titleFont);
 
-//    //Set up application version
-//    QLabel *appVersion = new QLabel(this);
-//    appVersion->setText(QString(tr("Version: ") + QString(version)));
-//    appVersion->setFont(aboutFont);
+    //    //Set up application version
+    //    QLabel *appVersion = new QLabel(this);
+    //    appVersion->setText(QString(tr("Version: ") + QString(version)));
+    //    appVersion->setFont(aboutFont);
 
-//    //Set up Git project link
-//    QLabel *gitUrlLabel = new QLabel(this);
-//    gitUrlLabel->setTextFormat(Qt::RichText);
-//    gitUrlLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
-//    gitUrlLabel->openExternalLinks();
-//    gitUrlLabel->setText("<a href='" + QString(git) + "'>" + tr("Git Project Page") + "</a>");
-//    connect(gitUrlLabel, &QLabel::linkActivated, this, &About::openGitUrl);
+    //    //Set up Git project link
+    //    QLabel *gitUrlLabel = new QLabel(this);
+    //    gitUrlLabel->setTextFormat(Qt::RichText);
+    //    gitUrlLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    //    gitUrlLabel->openExternalLinks();
+    //    gitUrlLabel->setText("<a href='" + QString(git) + "'>" + tr("Git Project Page") + "</a>");
+    //    connect(gitUrlLabel, &QLabel::linkActivated, this, &About::openGitUrl);
 
-//    //Set up the vertical box layout by adding the created widgets
-//    vBoxLayout->addWidget(appName);
-//    vBoxLayout->addWidget(appVersion);
-//    vBoxLayout->addWidget(gitUrlLabel);
+    //    //Set up the vertical box layout by adding the created widgets
+    //    vBoxLayout->addWidget(appName);
+    //    vBoxLayout->addWidget(appVersion);
+    //    vBoxLayout->addWidget(gitUrlLabel);
+
+    QDir *backResource = new QDir(":/background");
+    QVector<QLabel*> guiBGLabels {};
+    QVector<QRadioButton*> guiBGRadios {};
+
+    ui->gridLayout_2->setContentsMargins(50,50,50,50);
+
+    if(backResource->isEmpty() == false)
+    {
+        int itemNum {0};
+        //Set up background label and radio button defaults in GUI
+        for(unsigned int i{0};i<(backResource->count()*2)-1;i=i+2)
+        {
+            for(unsigned int j{0};j<(backResource->count()/5)+1;j++)
+            {
+                if(itemNum < backResource->count())
+                {
+                    QString labelName = "bgLabel_" + QString::number(itemNum);
+                    QLabel *label = new QLabel(labelName, this);
+                    label->setObjectName(labelName);
+                    label->setMinimumSize(100,100);
+                    label->setMaximumSize(100,100);
+                    label->setText("");
+                    label->setPixmap(QString(":/background/" + backResource->entryList().at(itemNum)));
+                    label->setScaledContents(true);
+                    label->setEnabled(true);
+                    label->setVisible(true);
+                    label->show();
+
+                    QString radioName = "bgRadio_" + QString::number(itemNum);
+                    QRadioButton *radio = new QRadioButton(radioName, this);
+                    radio->setObjectName(radioName);
+                    radio->setText("");
+                    radio->setEnabled(true);
+                    radio->setVisible(true);
+                    radio->show();
+
+                    ui->gridLayout_2->addWidget(label, i, j);
+                    ui->gridLayout_2->setAlignment(label, Qt::AlignCenter);
+
+                    ui->gridLayout_2->addWidget(radio, i+1, j);
+                    ui->gridLayout_2->setAlignment(radio, Qt::AlignCenter);
+
+                    guiBGLabels.append(label);
+                    guiBGRadios.append(radio);
+
+                    //Connect radio buttons with labels' resources
+
+                    itemNum++;
+                }
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Problem:  Resources not found." << std::endl;;
+    }
 }
 
 Preferences::~Preferences()
