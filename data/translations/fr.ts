234e9c1849e625db766488611bf6b25e1affbc3b
nightsky30 <6024315+nightsky30@users.noreply.github.com>
2019-10-23 21:03:19 -0500

    Translation:  First stab at French

---
+++ data/translations/fr.ts
@@ -0,0 +1,437 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../../src/about.cpp" line="36"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.cpp" line="52"/>
+        <source>Koi-Koi Hanafuda</source>
+        <translation>Koi-Koi Hanafuda</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.cpp" line="57"/>
+        <source>Version: </source>
+        <translation>Version: </translation>
+    </message>
+    <message>
+        <location filename="../../src/about.cpp" line="65"/>
+        <source>Git Project Page</source>
+        <translation>Page Projeter Git</translation>
+    </message>
+</context>
+<context>
+    <name>KoiKoi</name>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="29"/>
+        <location filename="../../ui/koikoi.ui" line="1661"/>
+        <location filename="../../src/koikoi.cpp" line="83"/>
+        <source>Koi-Koi Hanafuda</source>
+        <translation>Koi-Koi Hanafuda</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="156"/>
+        <source>Select a card!</source>
+        <translation>Choisissez une carte!</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="304"/>
+        <source>Round:  xx</source>
+        <translation>Ronde:  xx</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="328"/>
+        <location filename="../../ui/koikoi.ui" line="352"/>
+        <location filename="../../ui/koikoi.ui" line="391"/>
+        <location filename="../../ui/koikoi.ui" line="415"/>
+        <location filename="../../ui/koikoi.ui" line="439"/>
+        <location filename="../../ui/koikoi.ui" line="478"/>
+        <location filename="../../ui/koikoi.ui" line="544"/>
+        <location filename="../../ui/koikoi.ui" line="583"/>
+        <location filename="../../ui/koikoi.ui" line="607"/>
+        <location filename="../../ui/koikoi.ui" line="646"/>
+        <location filename="../../ui/koikoi.ui" line="670"/>
+        <location filename="../../ui/koikoi.ui" line="733"/>
+        <location filename="../../ui/koikoi.ui" line="772"/>
+        <location filename="../../ui/koikoi.ui" line="796"/>
+        <location filename="../../ui/koikoi.ui" line="820"/>
+        <location filename="../../ui/koikoi.ui" line="844"/>
+        <location filename="../../ui/koikoi.ui" line="868"/>
+        <location filename="../../ui/koikoi.ui" line="892"/>
+        <location filename="../../ui/koikoi.ui" line="946"/>
+        <location filename="../../ui/koikoi.ui" line="970"/>
+        <location filename="../../ui/koikoi.ui" line="1009"/>
+        <location filename="../../ui/koikoi.ui" line="1033"/>
+        <location filename="../../ui/koikoi.ui" line="1094"/>
+        <location filename="../../ui/koikoi.ui" line="1133"/>
+        <location filename="../../ui/koikoi.ui" line="1172"/>
+        <location filename="../../ui/koikoi.ui" line="1196"/>
+        <location filename="../../ui/koikoi.ui" line="1235"/>
+        <location filename="../../ui/koikoi.ui" line="1274"/>
+        <location filename="../../ui/koikoi.ui" line="1298"/>
+        <location filename="../../ui/koikoi.ui" line="1337"/>
+        <location filename="../../ui/koikoi.ui" line="1428"/>
+        <location filename="../../ui/koikoi.ui" line="1467"/>
+        <location filename="../../ui/koikoi.ui" line="1506"/>
+        <location filename="../../ui/koikoi.ui" line="1530"/>
+        <source>xx</source>
+        <translation>xx</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="370"/>
+        <source>Akatan Aotan No Chofuku (3 Red Poetry Ribbons and 3 Blue Ribbons)</source>
+        <translation>Akatan Aotan No Chofuku (3 Rubans Rouges De Poésie Et 3 Rubans Bleu)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="457"/>
+        <source>Tsukimi De Ippai (Moon and Sake Cup)</source>
+        <translation>Tsukimi De Ippai (La Lune Et Tasse De Saké)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="496"/>
+        <source>Play Again ??</source>
+        <translation>Jouez à Nouveau ??</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="520"/>
+        <location filename="../../ui/koikoi.ui" line="3343"/>
+        <source>Player</source>
+        <translation>Joueur</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="562"/>
+        <location filename="../../ui/koikoi.ui" line="2157"/>
+        <location filename="../../ui/koikoi.ui" line="2813"/>
+        <source>Ino-Shika-Cho (Boar-Deer-Butterflies)</source>
+        <translation>Ino-Shika-Cho (Le Verrat-Le Cerf-Les Papillons)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="688"/>
+        <source>Koi-Koi</source>
+        <translation>Koi-Koi</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="709"/>
+        <location filename="../../ui/koikoi.ui" line="3157"/>
+        <source>CPU</source>
+        <translation>CPU</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="751"/>
+        <location filename="../../ui/koikoi.ui" line="2045"/>
+        <location filename="../../ui/koikoi.ui" line="2701"/>
+        <source>Shiko (4 Brights)</source>
+        <translation>Shiko (4 Lumières)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="910"/>
+        <location filename="../../ui/koikoi.ui" line="2269"/>
+        <location filename="../../ui/koikoi.ui" line="2925"/>
+        <source>Aotan (Blue Ribbons)</source>
+        <translation>Aotan (Rubans Bleu)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="925"/>
+        <location filename="../../ui/koikoi.ui" line="2233"/>
+        <location filename="../../ui/koikoi.ui" line="2889"/>
+        <source>Tanzaku (Ribbons)</source>
+        <translation>Tanzaku (Rubans)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="988"/>
+        <location filename="../../ui/koikoi.ui" line="2359"/>
+        <location filename="../../ui/koikoi.ui" line="3019"/>
+        <source>Kasu (Plains)</source>
+        <translation>Kasu (Simples)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1066"/>
+        <source>Total</source>
+        <translation>Total</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1073"/>
+        <location filename="../../ui/koikoi.ui" line="3480"/>
+        <source>New Game</source>
+        <translation>Nouveau Jeu</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1112"/>
+        <source>Yaku</source>
+        <translation>Yaku</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1151"/>
+        <source>Subtotal</source>
+        <translation>Total Partiel</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1214"/>
+        <source>Ame-Shiko (4 Brights, Rain man)</source>
+        <translatorcomment>homme de pluie</translatorcomment>
+        <translation>Ame-Shiko (4 Lumières, Homme De Pluie)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1253"/>
+        <location filename="../../ui/koikoi.ui" line="2009"/>
+        <location filename="../../ui/koikoi.ui" line="2665"/>
+        <source>Sanko (3 Brights)</source>
+        <translation>Sanko (3 Lumières)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1316"/>
+        <location filename="../../ui/koikoi.ui" line="2251"/>
+        <location filename="../../ui/koikoi.ui" line="2907"/>
+        <source>Akatan (Red Poetry Ribbons)</source>
+        <translation>Akatan (Rubans Rouges De Poésie)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1355"/>
+        <location filename="../../ui/koikoi.ui" line="2139"/>
+        <location filename="../../ui/koikoi.ui" line="2795"/>
+        <source>Tane (Animals)</source>
+        <translation>Tane (Animaux)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1385"/>
+        <location filename="../../ui/koikoi.ui" line="2063"/>
+        <location filename="../../ui/koikoi.ui" line="2719"/>
+        <source>Goko (5 Brights)</source>
+        <translation>Goko (5 Lumières)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1400"/>
+        <source>Double Points (Opponent Declared Koi-Koi)</source>
+        <translation>Des Points Doubles (L&apos;Adversaire A Déclaré Koi-Koi)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1407"/>
+        <source>Continue</source>
+        <translation>Continuer</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1446"/>
+        <source>Hanami De Ippai (Curtain and Sake Cup)</source>
+        <translation>Hanami De Ippai (Le Rideau Et Tasse De Saké)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1795"/>
+        <source>Yes</source>
+        <translation>Oui</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1813"/>
+        <source>Koi-Koi?</source>
+        <translation>Koi-Koi?</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1820"/>
+        <source>No</source>
+        <translation>Non</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1945"/>
+        <location filename="../../ui/koikoi.ui" line="2604"/>
+        <source>Light</source>
+        <translation>Lumière</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1973"/>
+        <location filename="../../ui/koikoi.ui" line="2629"/>
+        <source>Tsukimi De Ippai</source>
+        <translation>Tsukimi De Ippai</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="1991"/>
+        <location filename="../../ui/koikoi.ui" line="2647"/>
+        <source>Hanami De Ippai</source>
+        <translation>Hanami De Ippai</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2027"/>
+        <location filename="../../ui/koikoi.ui" line="2683"/>
+        <source>Ame-Shiko (Rain man)</source>
+        <translation>Ame-Shiko (Homme De Pluie)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2114"/>
+        <location filename="../../ui/koikoi.ui" line="2770"/>
+        <source>Animal</source>
+        <translation>Animal</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2208"/>
+        <location filename="../../ui/koikoi.ui" line="2864"/>
+        <source>Ribbon</source>
+        <translation>Ruban</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2287"/>
+        <location filename="../../ui/koikoi.ui" line="2943"/>
+        <source>Akatan Aotan No Chofuku (3 Red Poetry, 3 Blue)</source>
+        <translation>Akatan Aotan No Chofuku (3 Rougees De Poésie, 3 Bleu)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2338"/>
+        <location filename="../../ui/koikoi.ui" line="2994"/>
+        <source>Plain / Misc</source>
+        <translation>Simple / Divers</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2377"/>
+        <location filename="../../ui/koikoi.ui" line="3037"/>
+        <source>Tsuki-fuda (Monthly Cards)</source>
+        <translation>Tsuki-fuda (Cartes Des Mois)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2395"/>
+        <location filename="../../ui/koikoi.ui" line="3055"/>
+        <source>Oya-Ken (Dealer&apos;s Privilege)</source>
+        <translation>Oya-Ken (Avantage Du Donneur)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="2413"/>
+        <location filename="../../ui/koikoi.ui" line="3073"/>
+        <source>Bake-Fuda (Wild Card)</source>
+        <translation>Bake-Fuda (Joker)</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3183"/>
+        <location filename="../../ui/koikoi.ui" line="3369"/>
+        <source>Score:  xx</source>
+        <translation>Score:  xx</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3207"/>
+        <location filename="../../ui/koikoi.ui" line="3399"/>
+        <source>Oya</source>
+        <translation>Oya</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3231"/>
+        <location filename="../../ui/koikoi.ui" line="3423"/>
+        <source>Koi-Koi: xx</source>
+        <translation>Koi-Koi: xx</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3304"/>
+        <source>Round: xx / xx</source>
+        <translation>Ronde: xx / xx</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3454"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3463"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3469"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3483"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3488"/>
+        <source>Quit Game</source>
+        <translation>Quittez Le Jeu</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3491"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3496"/>
+        <source>Exit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3499"/>
+        <source>Ctrl+X</source>
+        <translation>Ctrl+X</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3504"/>
+        <source>Preferences</source>
+        <translation>Préférences</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3507"/>
+        <source>Ctrl+P</source>
+        <translation>Ctrl+P</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3512"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="../../ui/koikoi.ui" line="3515"/>
+        <source>Ctrl+A</source>
+        <translation>Ctrl+A</translation>
+    </message>
+    <message>
+        <location filename="../../src/koikoi.cpp" line="591"/>
+        <source>Round: %1 / %2</source>
+        <translation>Ronde: %1 / %2</translation>
+    </message>
+    <message>
+        <location filename="../../src/koikoi.cpp" line="657"/>
+        <source>Round:  %1</source>
+        <translation>Ronde:  %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/koikoi.cpp" line="1100"/>
+        <location filename="../../src/koikoi.cpp" line="1101"/>
+        <source>Score:  %1</source>
+        <translation>Score:  %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/koikoi.cpp" line="1106"/>
+        <location filename="../../src/koikoi.cpp" line="1107"/>
+        <source>Koi-Koi: x%1</source>
+        <translation>Koi-Koi: x%1</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../../ui/preferences.ui" line="26"/>
+        <source>Koi-Koi Hanafuda - Preferences</source>
+        <translation>Koi-Koi Hanafuda - Préférences</translation>
+    </message>
+    <message>
+        <location filename="../../ui/preferences.ui" line="57"/>
+        <source>Misc.</source>
+        <translatorcomment>Divers ??</translatorcomment>
+        <translation>Misc.</translation>
+    </message>
+    <message>
+        <location filename="../../ui/preferences.ui" line="138"/>
+        <source>Rounds:</source>
+        <translation>Rondes:</translation>
+    </message>
+    <message>
+        <location filename="../../ui/preferences.ui" line="162"/>
+        <source>Decks</source>
+        <translation>Jeux</translation>
+    </message>
+    <message>
+        <location filename="../../ui/preferences.ui" line="194"/>
+        <source>Backgrounds</source>
+        <translation>Fonds</translation>
+    </message>
+</context>
+</TS>
